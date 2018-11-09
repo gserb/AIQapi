0a61c4ade0b74c43b6cb0b401f4105d9e536a86d
George Serbanescu <georgeserbanescu@hotmail.com>
2018-11-09 21:01:48 +0200

    try to fix heroku

--- app.js
+++ app.js
@@ -1,22 +1,128 @@
+var express = require('express');
+
+var app = express();
+const PORT = process.env.PORT || 8080;
+
 const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-node');
 
-// Load the binding:
-require('@tensorflow/tfjs-node');  // Use '@tensorflow/tfjs-node-gpu' if running with GPU.
+global.fetch = require('node-fetch');
+const fs = require('fs');
+const txt  = "RT  keithboykin: Thousands of protesters in New York s Times Square chant  Trump is not above the law.   "
+async function createModel()
+{
+    // const model = await tf.loadModel('file:///Users/ul14nw/Public/hackathonAIQ/model.json');
+  const model = await tf.loadModel('https://raw.githubusercontent.com/gserb/AIQapi/master/model.json');
+  return model
+}
 
-// Train a simple model:
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
-model.add(tf.layers.dense({units: 1, activation: 'linear'}));
-model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
+function processText(txt)
+{
+    out = txt.replace(/[^a-zA-Z0-9\s]/, '')
+    out = out.trim().split(/\s+/)
+    for (var i = 0 ; i < out.length ; i++)
+    	out[i] = out[i].toLowerCase()
+    return out
+}
 
-const xs = tf.randomNormal([100, 10]);
-const ys = tf.randomNormal([100, 1]);
+function loadDict()
+{
+    fs.readFile('dict.csv', 'utf8', function(err, data) {  
+        if (err) throw err;
+        success(data);
+    });
+}
 
-model.fit(xs, ys, {
-  epochs: 100,
-  callbacks: {
-    onEpochEnd: async (epoch, log) => {
-      console.log(`Epoch ${epoch}: loss = ${log.loss}`);
+function success(data)
+{
+    // load dictionary
+    var wd_idx = new Object();
+    lst = data.split(/\r?\n|\r/)
+
+    for(var i = 0 ; i < lst.length ;i++){
+        key = (lst[i]).split(',')[0]
+        value = (lst[i]).split(',')[1]
+        
+        if(key == "")
+            continue
+        wd_idx[key] = parseInt(value)
+        
     }
-  }
+    
+    word_index = wd_idx;
+
+    // create_sequences
+    max_tokens = 40 
+    tokens = []
+    words = processText(txt)
+    seq = Array.from(Array(max_tokens), () => 0) 
+    start = max_tokens-words.length;
+    if(!word_index) return;
+    for(var i= 0 ; i< words.length ; i++)
+    {
+        if (Object.keys(word_index).includes(words[i])){
+            seq[i+start] = word_index[words[i]]
+        }    
+        
+    }
+    return seq
+}
+
+// function create_sequences(txt)
+// {
+//     max_tokens = 40 
+//     tokens = []
+//     words = processText(txt)
+//     seq = Array.from(Array(max_tokens), () => 0) 
+//     start = max_tokens-words.length;
+//     if(!word_index) return;
+//     for(var i= 0 ; i< words.length ; i++)
+//     {
+//         if (Object.keys(word_index).includes(words[i])){
+//             seq[i+start] = word_index[words[i]]
+//         }    
+        
+//     }
+//     return seq
+// }
+
+
+async function init()
+{
+    console.log('Start loading dicionary')
+    // loadDict();
+    //console.log(word_index)
+    console.log('Finish loading dicionary')
+    seq = loadDict();
+    // seq = create_sequences(txt) 
+    console.log('Start loading model') 
+    model = await createModel()
+    console.log('Finish loading model') 
+    input = tf.tensor(seq)
+    input = input.expandDims(0)
+    pred = model.predict(input)
+    pred.print()
+
+    return pred.dataSync()[0];
+}
+
+word_index = undefined
+init()
+
+
+app.get('/api/sentiment', function(req, res) {
+
+  init().then(function(result) {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.send({
+      result,
+      text: txt
+    });
+  });
+  
 });
+
+
+// start the server
+app.listen(PORT);
+console.log('Server started! At http://localhost:' + PORT);
