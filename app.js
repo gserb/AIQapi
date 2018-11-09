0be05a7608e15d6a0ac74c7f6657ded3757c9f89
George Serbanescu <georgeserbanescu@hotmail.com>
2018-11-09 21:10:03 +0200

    update names of files

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-var express = require('express');
-
-var app = express();
-const PORT = process.env.PORT || 8080;
-
 const tf = require('@tensorflow/tfjs');
 require('@tensorflow/tfjs-node');
 
 global.fetch = require('node-fetch');
 const fs = require('fs');
-const txt  = "RT  keithboykin: Thousands of protesters in New York s Times Square chant  Trump is not above the law.   "
+const txt  = "Non-major bank ING has cut its fixed rates on investment loans by up to 36 basis points while lifting its fixed rates for owner-occupied";
+
+
 async function createModel()
 {
     // const model = await tf.loadModel('file:///Users/ul14nw/Public/hackathonAIQ/model.json');
@@ -16,7 +13,7 @@ async function createModel()
   return model
 }
 
-function processText(txt)
+function process(txt)
 {
     out = txt.replace(/[^a-zA-Z0-9\s]/, '')
     out = out.trim().split(/\s+/)
@@ -54,7 +51,7 @@ function success(data)
     // create_sequences
     max_tokens = 40 
     tokens = []
-    words = processText(txt)
+    words = process(txt)
     seq = Array.from(Array(max_tokens), () => 0) 
     start = max_tokens-words.length;
     if(!word_index) return;
@@ -68,26 +65,26 @@ function success(data)
     return seq
 }
 
-// function create_sequences(txt)
-// {
-//     max_tokens = 40 
-//     tokens = []
-//     words = processText(txt)
-//     seq = Array.from(Array(max_tokens), () => 0) 
-//     start = max_tokens-words.length;
-//     if(!word_index) return;
-//     for(var i= 0 ; i< words.length ; i++)
-//     {
-//         if (Object.keys(word_index).includes(words[i])){
-//             seq[i+start] = word_index[words[i]]
-//         }    
+function create_sequences(txt)
+{
+    max_tokens = 40 
+    tokens = []
+    words = process(txt)
+    seq = Array.from(Array(max_tokens), () => 0) 
+    start = max_tokens-words.length;
+    if(!word_index) return;
+    for(var i= 0 ; i< words.length ; i++)
+    {
+        if (Object.keys(word_index).includes(words[i])){
+            seq[i+start] = word_index[words[i]]
+        }    
         
-//     }
-//     return seq
-// }
+    }
+    return seq
+}
 
 
-async function init()
+async function init(model)
 {
     console.log('Start loading dicionary')
     // loadDict();
@@ -102,27 +99,7 @@ async function init()
     input = input.expandDims(0)
     pred = model.predict(input)
     pred.print()
-
-    return pred.dataSync()[0];
 }
 
 word_index = undefined
 init()
-
-
-app.get('/api/sentiment', function(req, res) {
-
-  init().then(function(result) {
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.send({
-      result,
-      text: txt
-    });
-  });
-  
-});
-
-
-// start the server
-app.listen(PORT);
-console.log('Server started! At http://localhost:' + PORT);
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+var express = require('express');
+
+var app = express();
+const PORT = process.env.PORT || 8080;
+
 const tf = require('@tensorflow/tfjs');
 require('@tensorflow/tfjs-node');
 
 global.fetch = require('node-fetch');
 const fs = require('fs');
-const txt  = "Non-major bank ING has cut its fixed rates on investment loans by up to 36 basis points while lifting its fixed rates for owner-occupied";
-
-
+const txt  = "RT  keithboykin: Thousands of protesters in New York s Times Square chant  Trump is not above the law.   "
 async function createModel()
 {
     // const model = await tf.loadModel('file:///Users/ul14nw/Public/hackathonAIQ/model.json');
@@ -13,7 +16,7 @@ async function createModel()
   return model
 }
 
-function process(txt)
+function processText(txt)
 {
     out = txt.replace(/[^a-zA-Z0-9\s]/, '')
     out = out.trim().split(/\s+/)
@@ -51,7 +54,7 @@ function success(data)
     // create_sequences
     max_tokens = 40 
     tokens = []
-    words = process(txt)
+    words = processText(txt)
     seq = Array.from(Array(max_tokens), () => 0) 
     start = max_tokens-words.length;
     if(!word_index) return;
@@ -65,26 +68,26 @@ function success(data)
     return seq
 }
 
-function create_sequences(txt)
-{
-    max_tokens = 40 
-    tokens = []
-    words = process(txt)
-    seq = Array.from(Array(max_tokens), () => 0) 
-    start = max_tokens-words.length;
-    if(!word_index) return;
-    for(var i= 0 ; i< words.length ; i++)
-    {
-        if (Object.keys(word_index).includes(words[i])){
-            seq[i+start] = word_index[words[i]]
-        }    
+// function create_sequences(txt)
+// {
+//     max_tokens = 40 
+//     tokens = []
+//     words = processText(txt)
+//     seq = Array.from(Array(max_tokens), () => 0) 
+//     start = max_tokens-words.length;
+//     if(!word_index) return;
+//     for(var i= 0 ; i< words.length ; i++)
+//     {
+//         if (Object.keys(word_index).includes(words[i])){
+//             seq[i+start] = word_index[words[i]]
+//         }    
         
-    }
-    return seq
-}
+//     }
+//     return seq
+// }
 
 
-async function init(model)
+async function init()
 {
     console.log('Start loading dicionary')
     // loadDict();
@@ -99,7 +102,27 @@ async function init(model)
     input = input.expandDims(0)
     pred = model.predict(input)
     pred.print()
+
+    return pred.dataSync()[0];
 }
 
 word_index = undefined
 init()
+
+
+app.get('/api/sentiment', function(req, res) {
+
+  init().then(function(result) {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.send({
+      result,
+      text: txt
+    });
+  });
+  
+});
+
+
+// start the server
+app.listen(PORT);
+console.log('Server started! At http://localhost:' + PORT);
